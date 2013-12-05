de8a894fafac4f6ec082aa518f8e73f54106a482
KTerrell13 <kevinterrell13@gmail.com>
2013-12-05 14:56:38 -0600

    solve first test

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require 'fizzbuzz.rb'
+
+describe 'Calculate class' do
+    it 'should return 1 for 1' do
+    @calculate = Calculator.new
+    @calculate.calculate(1).should == 1	
+end
